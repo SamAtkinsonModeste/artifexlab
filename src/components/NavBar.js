b74bdbae150f31a7ab3398ae76468212d4d3937e
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-11 18:37:26 +0100

    refactor: component NavBar :replace Nav.Link placed Bootstrap Nav.Link to reacts NavLink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,31 +3,116 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/artifexLab-logo.svg";
 import styles from "../styles/NavBar.module.css";
 import Design from "../styles/Design.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" className="w-sm-80 px-3 w-md-100">
+    <Navbar expand="lg" className="w-sm-80 px-3 w-md-100">
       <Container fluid>
-        <div>
-          <Navbar.Brand>
-            <img
-              src={logo}
-              alt="ArtifexLab Logo"
-              className={styles.NavbarLogo}
-              height="60"
-            />
-          </Navbar.Brand>
-        </div>
+        <Navbar.Brand>
+          <img
+            src={logo}
+            alt="ArtifexLab Logo"
+            className={styles.NavbarLogo}
+            height="60"
+          />
+        </Navbar.Brand>
+
         <Navbar.Toggle
           aria-controls="navbar-nav"
           className={`${Design.bgMainGradient} ${Design.textWhiteLight}`}
         />
 
         <Navbar.Collapse id="navbar-nav" className="mt-2">
-          <Nav className={`${styles.NavLab}  ms-auto rounded ps-3`}>
+          <Nav className={`${styles.NavLab} ms-auto rounded ps-3`}>
+            {/* Home */}
+            <NavLink
+              to="/"
+              className={`order-1 order-md-0 ${styles.NavLink} ${styles.NavFade} ${styles.delay2} activeClassName=${styles.Active}`}
+            >
+              Home
+            </NavLink>
+
+            {/* Artworks */}
+            <NavLink
+              to="/artworks"
+              className={`order-2 order-md-1 ${styles.NavLink} ${styles.NavFade} ${styles.delay3} activeClassName=${styles.Active}`}
+            >
+              Artworks
+            </NavLink>
+
+            {/* Tutorials */}
+            <NavDropdown
+              title={<span className={styles.NavLink}>Tutorials</span>}
+              id="tutorials-nav-dropdown"
+              className={`order-3 order-md-2 ${styles.NavFade} ${styles.delay4} w-50`}
+            >
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/view-tutorials"
+              >
+                View Tutorials
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/tutorial-results"
+              >
+                Tutorial Results
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* Feed */}
+            <NavLink
+              to="/feed"
+              className={`order-4 order-md-3 ${styles.NavLink} ${styles.NavFade} ${styles.delay5} activeClassName=${styles.Active}`}
+            >
+              Feed
+            </NavLink>
+
+            {/* Create - this will be hidden for logged-out users later */}
+            <NavDropdown
+              title={<span className={styles.NavLink}>Create</span>}
+              id="create-nav-dropdown"
+              className={`order-5 order-md-4  ${styles.NavFade} ${styles.delay6}   w-50`}
+            >
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/upload-artwork"
+              >
+                Upload Artwork
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/create-tutorials"
+              >
+                Create a Tutorial
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            {/* Sign In */}
+            <NavLink
+              to="/signin"
+              className={` order-6 order-md-5 ps-md-4 ${styles.NavLink} ${styles.NavFade} ${styles.delay7} text-nowrap`}
+            >
+              Sign In
+            </NavLink>
+
+            {/* Register */}
+            <NavLink
+              to="/register"
+              className={`order-7 order-md-6 ${styles.NavLink} ${styles.NavFade} ${styles.delay8}`}
+            >
+              Register
+            </NavLink>
+
+            {/* User dropdown - this will be hidden for logged-out users later */}
             <NavDropdown
               title={
                 <div className="bg-primary rounded-circle p-1 d-inline-block">
@@ -41,47 +126,30 @@ const NavBar = () => {
                 </div>
               }
               id="user-nav-dropdown"
-              className={`order-0 order-md-5 ${styles.NavFade} ${styles.DelayOne} ${Design.bgWhiteBase} w-75`}
+              className={`order-6 order-md-5  ${styles.NavFade} ${styles.DelayOne} ${Design.bgWhiteBase} w-75`}
             >
-              <NavDropdown.Item>My Profile</NavDropdown.Item>
-              <NavDropdown.Item>Edit Profile</NavDropdown.Item>
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/profile"
+              >
+                My Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/edit-profile"
+              >
+                Edit Profile
+              </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item>Sign Out</NavDropdown.Item>
-            </NavDropdown>
-
-            <Nav.Link
-              className={`order-1 order-md-0 ${styles.NavFade} ${styles.delay2} `}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              className={`order-2 order-md-1 ${styles.NavFade} ${styles.delay3} `}
-            >
-              Artworks
-            </Nav.Link>
-
-            <NavDropdown
-              title="Tutorials"
-              id="tutorials-nav-dropdown"
-              className={`order-3 order-md-2 ${styles.NavFade} ${styles.delay4}  w-75`}
-            >
-              <NavDropdown.Item>View Tutorials</NavDropdown.Item>
-              <NavDropdown.Item>Tutorial Attempts</NavDropdown.Item>
-            </NavDropdown>
-
-            <Nav.Link
-              className={`order-4 order-md-3 ${styles.NavFade} ${styles.delay5}`}
-            >
-              Feed
-            </Nav.Link>
-
-            <NavDropdown
-              title="Create"
-              id="create-nav-dropdown"
-              className={`order-5 order-md-4 ${styles.NavFade} ${styles.delay6}  pe-md-5 w-75`}
-            >
-              <NavDropdown.Item>Upload Artwork</NavDropdown.Item>
-              <NavDropdown.Item>Create a Tutorial</NavDropdown.Item>
+              <NavDropdown.Item
+                className={`${styles.NavLink} activeClassName=${styles.Active} `}
+                as={NavLink}
+                to="/signin"
+              >
+                Sign Out
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
