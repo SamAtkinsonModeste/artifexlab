4b0c00aaec28ecc261ddc87f67e21f7e03464ff1
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-11 18:42:35 +0100

    feat: add React Router routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -9,8 +10,13 @@ function App() {
         <NavBar />
       </header>
       <Container fluid className={styles.Main}>
-        <h1>Home Page</h1>
-        <h1>Sign In</h1>
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={<h1>Sign In</h1>} />
+          <Route path="/" element={<h1>Register</h1>} />
+          <Route path="artworks/" element={<h1>Artworks</h1>} />
+          <Route path="*" element={<p> Page Not Found </p>} />
+        </Routes>
       </Container>
     </div>
   );
