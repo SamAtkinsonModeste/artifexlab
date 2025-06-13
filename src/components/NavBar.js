c06fb1125d8dac7bc8c5227aa923a72c70b3a6c0
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-14 00:40:12 +0100

    feat(components): NavBar.js add auth-based display
    Connected NavBar to CurrentUserContext, Created loggedIn and loggedOut variables,  Displayed different nav links and icons depending on auth state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,14 +7,88 @@ import { NavLink } from "react-router-dom";
 import logo from "../assets/artifexLab-logo.svg";
 import styles from "../styles/NavBar.module.css";
 import Design from "../styles/Design.module.css";
+import { CurrentUserContext } from "../App";
 // import Avatar from "./Avatar";
 
 const NavBar = () => {
-  const [expanded, setExpanded] = useState();
-
+  const currentUser = useContext(CurrentUserContext);
   const handleLinkClick = () => {
     setExpanded(false);
   };
+
+  const loggedInIcons = (
+    <>
+      {/* User dropdown - this will be hidden for logged-out users later */}
+      <NavDropdown
+        title={
+          <div className="position-relative d-inline-block">
+            <span className="d-inline-block px-2">{currentUser?.username}</span>
+            <i
+              className="fas fa-circle-notch text-success position-absolute bottom-0 end-0"
+              style={{ fontSize: "0.6rem" }}
+            ></i>
+          </div>
+        }
+        id="user-nav-dropdown"
+        className={`order-6 order-md-5  ${styles.NavFade} ${styles.DelayOne} ${Design.bgWhiteBase} w-75`}
+      >
+        <NavDropdown.Item
+          className={`${styles.NavLink} activeClassName=${styles.Active} `}
+          as={NavLink}
+          onClick={handleLinkClick}
+          to="/profile"
+        >
+          My Profile
+        </NavDropdown.Item>
+        <NavDropdown.Item
+          className={`${styles.NavLink} activeClassName=${styles.Active} `}
+          as={NavLink}
+          onClick={handleLinkClick}
+          to="/edit-profile"
+        >
+          Edit Profile
+        </NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item
+          className={`${styles.NavLink} activeClassName=${styles.Active} `}
+          as={NavLink}
+          onClick={handleLinkClick}
+          to="/signin"
+        >
+          Sign Out
+        </NavDropdown.Item>
+      </NavDropdown>
+    </>
+  );
+
+  const loggedOutIcons = (
+    <>
+      {/* Sign In */}
+      <NavLink
+        to="/signin"
+        className={` order-6 order-md-5 ps-md-4 ${styles.NavLink} ${styles.NavFade} ${styles.delay7} text-nowrap`}
+        onClick={handleLinkClick}
+      >
+        <span className={`${styles.SigninupIcons} me-2`}>
+          <i className="fas fa-right-to-bracket"></i>
+        </span>
+        Sign In
+      </NavLink>
+
+      {/* Sign Up */}
+      <NavLink
+        to="/signup"
+        className={`order-7 order-md-6 text-nowrap ${styles.NavLink} ${styles.NavFade} ${styles.delay8}`}
+        onClick={handleLinkClick}
+      >
+        <span className={`${styles.SigninupIcons} me-2`}>
+          <i className="fas fa-id-badge"></i>
+        </span>
+        Sign Up
+      </NavLink>
+    </>
+  );
+  const [expanded, setExpanded] = useState();
 
   return (
     <Navbar
@@ -23,7 +97,7 @@ const NavBar = () => {
       onToggle={setExpanded}
       className="w-sm-80 px-3 w-md-100"
     >
-      <Container fluid className="flex-nowrap">
+      <Container fluid>
         <Navbar.Brand>
           <img
             src={logo}
@@ -115,56 +189,7 @@ const NavBar = () => {
               </NavDropdown.Item>
             </NavDropdown>
 
-            {/* Sign In */}
-            <NavLink
-              to="/signin"
-              className={` order-6 order-md-5 ps-md-4 ${styles.NavLink} ${styles.NavFade} ${styles.delay7} text-nowrap`}
-              onClick={handleLinkClick}
-            >
-              Sign In
-            </NavLink>
-
-            {/* Sign Up */}
-            <NavLink
-              to="/signup"
-              className={`order-7 order-md-6 text-nowrap ${styles.NavLink} ${styles.NavFade} ${styles.delay8}`}
-              onClick={handleLinkClick}
-            >
-              Sign Up
-            </NavLink>
-
-            {/* User dropdown - this will be hidden for logged-out users later */}
-            <NavDropdown
-              title="Profile"
-              id="user-nav-dropdown"
-              className={`order-6 order-md-5  ${styles.NavFade} ${styles.DelayOne} ${Design.bgWhiteBase} w-75`}
-            >
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/profile"
-              >
-                My Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/edit-profile"
-              >
-                Edit Profile
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/signin"
-              >
-                Sign Out
-              </NavDropdown.Item>
-            </NavDropdown>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
