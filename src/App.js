88e351e176b2aaa0f791353c5dfb112428701ba5
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-13 14:42:07 +0100

    Revert "feat: add initial routes and imports to App.js"
    This reverts commit f8ee4388ade61ceb44652a3682a295e3883a50cc.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import Container from "react-bootstrap/Container";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
-import "./api/axiosDefaults";
-import SignUpForm from "./pages/auth/SignUpForm";
-import Home from "./pages/Home/Home";
-import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,17 +11,13 @@ function App() {
       </header>
       <Container fluid className={styles.Main}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<h1>Sign In</h1>} />
-          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/" element={<h1>Sign In</h1>} />
+          <Route path="/" element={<h1>Register</h1>} />
           <Route path="artworks/" element={<h1>Artworks</h1>} />
-          <Route path="profiles/" element={<h1>Profiles</h1>} />
           <Route path="*" element={<p> Page Not Found </p>} />
         </Routes>
       </Container>
-      <footer>
-        <Footer />
-      </footer>
     </div>
   );
 }
