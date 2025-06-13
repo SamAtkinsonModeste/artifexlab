d1bad7763157e10519b587a937a6cdb675c3c039
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-13 22:02:02 +0100

    feat(context): add CurrentContext.Provider to App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ import { Routes, Route } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignInForm from "./pages/auth/SignInForm";
 import SignUpForm from "./pages/auth/SignUpForm";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const response = await axios.get("/dj-rest-auth/user/");
+      setCurrentUser(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
   return (
-    <div className={styles.App}>
-      <header className={styles.Header}>
-        <NavBar />
-      </header>
-      <Container fluid className={styles.Main}>
-        <Routes>
-          <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/signin" element={<SignInForm />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/artworks" element={<h1>Artworks</h1>} />
-          <Route path="*" element={<p> Page Not Found </p>} />
-        </Routes>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <header className={styles.Header}>
+            <NavBar />
+          </header>
+          <Container fluid className={styles.Main}>
+            <Routes>
+              <Route path="/" element={<h1>Home Page</h1>} />
+              <Route path="/signin" element={<SignInForm />} />
+              <Route path="/signup" element={<SignUpForm />} />
+              <Route path="/artworks" element={<h1>Artworks</h1>} />
+              <Route path="*" element={<p> Page Not Found </p>} />
+            </Routes>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
