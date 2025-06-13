b27f3ebde94a9226de42b5f852745b7ad9834830
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-13 20:49:48 +0100

    feat(pages): Added SignIn & SignOut routes
    SignInForm & SignUpForms .js imported and added as routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Container from "react-bootstrap/Container";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignInForm from "./pages/auth/SignInForm";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -12,9 +15,9 @@ function App() {
       <Container fluid className={styles.Main}>
         <Routes>
           <Route path="/" element={<h1>Home Page</h1>} />
-          <Route path="/" element={<h1>Sign In</h1>} />
-          <Route path="/" element={<h1>Register</h1>} />
-          <Route path="artworks/" element={<h1>Artworks</h1>} />
+          <Route path="/signin" element={<SignInForm />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/artworks" element={<h1>Artworks</h1>} />
           <Route path="*" element={<p> Page Not Found </p>} />
         </Routes>
       </Container>
