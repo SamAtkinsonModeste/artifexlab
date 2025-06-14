b905ebe7e93577ff0023c9aba3c77a430fb7b277
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-14 16:51:09 +0100

    refactor(components): update NavBar user state logic
    Removed useContext and direct context references - Imported useCurrentUser and useSetCurrentUser hooks - Added variables to conditionally render nav items based on user auth state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,58 +7,149 @@ import { NavLink } from "react-router-dom";
 import logo from "../assets/artifexLab-logo.svg";
 import styles from "../styles/NavBar.module.css";
 import Design from "../styles/Design.module.css";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
+import axios from "axios";
+import Avatar from "./Avatar";
 
 // import Avatar from "./Avatar";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const [expanded, setExpanded] = useState();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const handleLinkClick = () => {
     setExpanded(false);
   };
 
   const loggedInIcons = (
     <>
-      {/* User dropdown - this will be hidden for logged-out users later */}
+      {/* Feed */}
+      <NavLink
+        to="/feed"
+        className={`order-5 order-md-3 ${styles.NavLink} ${styles.NavFade} ${styles.delay4} activeClassName=${styles.Active}`}
+        onClick={handleLinkClick}
+      >
+        <span className={styles.NavBarIcons}>
+          <i className="fas fa-stream"></i>
+        </span>
+        Feed
+      </NavLink>
+
+      {/* Tutorial Results */}
+      <NavLink
+        to="/tutorial-results"
+        className={`order-5 order-md-4 text-nowrap ${styles.NavLink} ${styles.NavFade} ${styles.delay5} activeClassName=${styles.Active}`}
+        onClick={handleLinkClick}
+      >
+        <span className={styles.NavBarIcons}>
+          <i className="fas fa-clipboard-check"></i>
+        </span>{" "}
+        Tutorial Results
+      </NavLink>
+
+      {/* Create Dropdown */}
       <NavDropdown
         title={
-          <div className="position-relative d-inline-block">
-            <span className="d-inline-block px-2">{currentUser?.username}</span>
-            <i
-              className="fas fa-circle-notch text-success position-absolute bottom-0 end-0"
-              style={{ fontSize: "0.6rem" }}
-            ></i>
-          </div>
+          <>
+            <span className={styles.NavBarIcons}>
+              <i className="fas fa-plus"></i>
+            </span>{" "}
+            <span className={styles.NavLink}>Create</span>
+          </>
         }
-        id="user-nav-dropdown"
-        className={`order-6 order-md-5  ${styles.NavFade} ${styles.DelayOne} ${Design.bgWhiteBase} w-75`}
+        id="create-nav-dropdown"
+        className={`order-5 order-md-5 ${styles.NavFade} ${styles.delay6} ${styles.Dropdown}`}
       >
         <NavDropdown.Item
-          className={`${styles.NavLink} activeClassName=${styles.Active} `}
+          className={`${styles.NavLink} ${styles.Dropdown} activeClassName=${styles.Active}`}
           as={NavLink}
           onClick={handleLinkClick}
-          to="/profile"
+          to="/upload-artwork"
         >
-          My Profile
+          <span className={styles.NavBarIcons}>
+            <i className="fas fa-paint-brush"></i>
+          </span>
+          Upload Artwork
         </NavDropdown.Item>
+
         <NavDropdown.Item
-          className={`${styles.NavLink} activeClassName=${styles.Active} `}
+          className={`${styles.NavLink} activeClassName=${styles.Active} ${styles.Dropdown}`}
           as={NavLink}
           onClick={handleLinkClick}
-          to="/edit-profile"
+          to="/create-tutorials"
         >
-          Edit Profile
-        </NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item
-          className={`${styles.NavLink} activeClassName=${styles.Active} `}
-          as={NavLink}
-          onClick={handleLinkClick}
-          to="/signin"
-        >
-          Sign Out
+          <span className={styles.NavBarIcons}>
+            <i className="fas fa-file-alt"></i>
+          </span>
+          Create a Tutorial
         </NavDropdown.Item>
       </NavDropdown>
+
+      {/* User Dropdown */}
+      <Nav.Item
+        className={`order-0 order-md-6 order-lg-6 ${styles.ProfileNav}`}
+      >
+        <NavDropdown
+          title={
+            <div
+              className={styles.NavAvatarWrap}
+              style={{ position: "relative" }}
+            >
+              <div className={styles.AvatarContainer}>
+                <Avatar
+                  src={currentUser?.profile_image}
+                  text="Profile"
+                  height={40}
+                />
+                <i
+                  className={`fas fa-circle-notch ${styles.NavAvatarStatus}${styles.Dropdown}`}
+                ></i>
+              </div>
+            </div>
+          }
+          id="user-nav-dropdown"
+          className={`${styles.NavFade} ${styles.DelayOne} ${Design.bgWhiteBase}`}
+        >
+          <NavDropdown.Item
+            className={`${styles.NavLink} activeClassName=${styles.Active} ${styles.Dropdown}`}
+            as={NavLink}
+            onClick={handleLinkClick}
+            to={`/profiles/${currentUser?.profile_id}`}
+          >
+            My Profile
+          </NavDropdown.Item>
+          <NavDropdown.Item
+            className={`${styles.NavLink} activeClassName=${styles.Active} ${styles.Dropdown}`}
+            as={NavLink}
+            onClick={handleLinkClick}
+            to={`/profiles/${currentUser?.profile_id}`}
+          >
+            Edit Profile
+          </NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item
+            className={`${styles.NavLink} activeClassName=${styles.Active} ${styles.ActiveSignOut}`}
+            as={NavLink}
+            onClick={handleSignOut}
+            to="/"
+          >
+            Sign Out
+          </NavDropdown.Item>
+        </NavDropdown>
+      </Nav.Item>
     </>
   );
 
@@ -67,10 +158,10 @@ const NavBar = () => {
       {/* Sign In */}
       <NavLink
         to="/signin"
-        className={` order-6 order-md-5 ps-md-4 ${styles.NavLink} ${styles.NavFade} ${styles.delay7} text-nowrap`}
+        className={`order-4 order-md-3 ps-md-4 ${styles.NavLink} ${styles.NavFade} ${styles.delay7} text-nowrap`}
         onClick={handleLinkClick}
       >
-        <span className={`${styles.SigninupIcons} me-2`}>
+        <span className={`${styles.NavBarIcons} me-2`}>
           <i className="fas fa-right-to-bracket"></i>
         </span>
         Sign In
@@ -79,17 +170,16 @@ const NavBar = () => {
       {/* Sign Up */}
       <NavLink
         to="/signup"
-        className={`order-7 order-md-6 text-nowrap ${styles.NavLink} ${styles.NavFade} ${styles.delay8}`}
+        className={`order-5 order-md-4 text-nowrap ${styles.NavLink} ${styles.NavFade} ${styles.delay8}`}
         onClick={handleLinkClick}
       >
-        <span className={`${styles.SigninupIcons} me-2`}>
+        <span className={`${styles.NavBarIcons} me-2`}>
           <i className="fas fa-id-badge"></i>
         </span>
         Sign Up
       </NavLink>
     </>
   );
-  const [expanded, setExpanded] = useState();
 
   return (
     <Navbar
@@ -118,7 +208,7 @@ const NavBar = () => {
             {/* Home */}
             <NavLink
               to="/"
-              className={`order-1 order-md-0 ${styles.NavLink} ${styles.NavFade} ${styles.delay2} activeClassName=${styles.Active}`}
+              className={`order-1 order-md-0 ${styles.NavLink} ${styles.NavFade} ${styles.delay1} activeClassName=${styles.Active}`}
               onClick={handleLinkClick}
             >
               Home
@@ -127,68 +217,20 @@ const NavBar = () => {
             {/* Artworks */}
             <NavLink
               to="/artworks"
-              className={`order-2 order-md-1 ${styles.NavLink} ${styles.NavFade} ${styles.delay3} activeClassName=${styles.Active}`}
+              className={`order-3 order-md-1 ${styles.NavLink} ${styles.NavFade} ${styles.delay2} activeClassName=${styles.Active}`}
               onClick={handleLinkClick}
             >
               Artworks
             </NavLink>
 
-            {/* Tutorials */}
-            <NavDropdown
-              title={<span className={styles.NavLink}>Tutorials</span>}
-              id="tutorials-nav-dropdown"
-              className={`order-3 order-md-2 ${styles.NavFade} ${styles.delay4} w-50`}
-            >
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/view-tutorials"
-              >
-                View Tutorials
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/tutorial-results"
-              >
-                Tutorial Results
-              </NavDropdown.Item>
-            </NavDropdown>
-
-            {/* Feed */}
+            {/* View Tutorials */}
             <NavLink
-              to="/feed"
-              className={`order-4 order-md-3 ${styles.NavLink} ${styles.NavFade} ${styles.delay5} activeClassName=${styles.Active}`}
+              to="/tutorials"
+              className={`order-4 order-md-2 text-nowrap ${styles.NavLink} ${styles.NavFade} ${styles.delay3} activeClassName=${styles.Active}`}
               onClick={handleLinkClick}
             >
-              Feed
+              View Tutorials
             </NavLink>
-
-            {/* Create - this will be hidden for logged-out users later */}
-            <NavDropdown
-              title={<span className={styles.NavLink}>Create</span>}
-              id="create-nav-dropdown"
-              className={`order-5 order-md-4  ${styles.NavFade} ${styles.delay6}   w-50`}
-            >
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/upload-artwork"
-              >
-                Upload Artwork
-              </NavDropdown.Item>
-              <NavDropdown.Item
-                className={`${styles.NavLink} activeClassName=${styles.Active} `}
-                as={NavLink}
-                onClick={handleLinkClick}
-                to="/create-tutorials"
-              >
-                Create a Tutorial
-              </NavDropdown.Item>
-            </NavDropdown>
 
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
