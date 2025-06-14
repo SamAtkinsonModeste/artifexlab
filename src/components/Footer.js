16e2c3cb70473913ae282c4a73bd15159daec7c3
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-14 18:51:27 +0100

    feat(components): created Footer
    The footer contains the copy right and social media links

---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Design from "../styles/UniversalDesign.module.css";
+
+const Footer = () => {
+  return (
+    <Container fluid className={` ${Design.Footer}`}>
+      <Container className="mt-auto text-center py-2">
+        <p
+          className={`mb-1 fst-italic ${Design.fontPinotMed}  ${Design.TaglineSmall}`}
+        >
+          “Create. Inspire. Mentor.”
+        </p>
+        <div className="d-flex justify-content-center gap-3 my-3">
+          <span className={Design.FooterSocialIcons}>
+            <Nav.Link
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon instagram"
+            >
+              <i className="fab fa-instagram"></i>
+            </Nav.Link>
+          </span>
+          <span className={Design.FooterSocialIcons}>
+            <Nav.Link
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon facebook"
+            >
+              <i className="fab fa-facebook"></i>
+            </Nav.Link>
+          </span>
+          <span className={Design.FooterSocialIcons}>
+            <Nav.Link
+              href="https://x.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="social-icon twitter"
+            >
+              <i className="fab fa-x-twitter"></i>
+            </Nav.Link>
+          </span>
+        </div>
+
+        <small className={`${Design.FooterCopySmall}`}>
+          © 2025 ArtifexLab. All rights reserved.
+        </small>
+      </Container>
+    </Container>
+  );
+};
+
+export default Footer;
