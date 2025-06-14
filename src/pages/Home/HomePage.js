79b97a700f3260ed515c9c75adf70ff247d60cf5
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-06-14 22:45:05 +0100

    feat(pages): Created HomePage.js ArtifexLab's home page

---
+++ src/pages/Home/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from "./HeroSection.js";
+import AboutSection from "./AboutSection.js";
+import FeaturedContent from "./FeaturedContent.js";
+
+const HomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection />
+      <FeaturedContent />
+    </>
+  );
+};
+
+export default HomePage;
