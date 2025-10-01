d5551c33aa321b9e0f04cdf31ff09688b9e8adcb
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-10-01 19:01:23 +0100

    feat(avatar): add default image and gradient wrapper

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,24 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
+import uniStyles from "../styles/UniversalDesign.module.css";
 
 const Avatar = ({ src, height = 45, text }) => {
+  const defaultAvatar =
+    "https://res.cloudinary.com/dpnxbddic/image/upload/v1/media/../default_profile_us7dxw_phmfan.jpg";
+
   return (
-    <span>
-      <img
-        className={styles.Avatar}
-        src={src}
-        height={height}
-        width={height}
-        alt="avatar"
-      />
-      {text}
-    </span>
+    <div className={`${styles.AvatarWrapper} ${uniStyles.bgMainGradient}`}>
+      <span>
+        <img
+          className={styles.Avatar}
+          src={src || defaultAvatar}
+          height={height}
+          width={height}
+          alt="avatar"
+        />
+        {text}
+      </span>
+    </div>
   );
 };
 
