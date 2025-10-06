20a6d4f5f38dc5267ce4692e95e705ada3b1dc4a
Sam Atkinson Modeste <sammodeste@yahoo.com>
2025-10-06 18:25:54 +0100

    refactor(component): rename content field to description in ArtUploadForm

--- src/pages/artworks/ArtUploadForm.js
+++ src/pages/artworks/ArtUploadForm.js
@@ -1,21 +1,30 @@
 import React, { useRef, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+
+import Upload from "../../assets/upload.svg";
 import { useNavigate } from "react-router-dom";
 import FieldAlerts from "../../components/FieldAlerts";
 import { axiosReq } from "../../api/axiosDefaults";
+
+import appStyles from "../../App.module.css";
+
+import uniStyles from "../../styles/UniversalDesign.module.css";
+import styles from "../../styles/ArtUpLoadForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import design from "../../styles/UniversalDesign.module.css";
+import Asset from "../../components/Asset";
 
 const ArtUploadForm = () => {
   const [artworkData, setArtworkData] = useState({
     title: "",
-    content: "",
+    description: "",
     image: "",
   });
 
-  const { title, content, image } = artworkData;
+  const { title, description, image } = artworkData;
   const [errors, setErrors] = useState({});
 
   const imageInput = useRef(null);
@@ -43,7 +52,7 @@ const ArtUploadForm = () => {
   const handleCancel = () => {
     setArtworkData({
       title: "",
-      content: "",
+      description: "",
       image: "",
     });
     setErrors({});
@@ -55,13 +64,13 @@ const ArtUploadForm = () => {
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     console.log("Title:", title);
-    console.log("Content:", content);
+    console.log("Description:", description);
     console.log("Image file:", imageInput.current?.files[0]);
 
     const formData = new FormData();
 
     formData.append("title", title);
-    formData.append("content", content);
+    formData.append("description", description);
     formData.append("image", imageInput.current.files[0]);
 
     try {
@@ -79,6 +88,7 @@ const ArtUploadForm = () => {
   const formFields = (
     <>
       {/* Title Field */}
+
       <Form.Group controlId="artTitle">
         <Form.Label>Title</Form.Label>
         <Form.Control
@@ -92,87 +102,110 @@ const ArtUploadForm = () => {
       </Form.Group>
 
       {/* Content Field */}
-      <Form.Group controlId="artContent" className="mt-3">
+      <Form.Group controlId="artContent" className="mt-3 mx-auto">
         <Form.Label>Content</Form.Label>
         <Form.Control
           as="textarea"
           rows={6}
-          name="content"
+          name="description"
           placeholder="Tell us about your artwork..."
-          value={content}
+          value={description}
           onChange={handleChange}
         />
-        <FieldAlerts messages={errors?.content} />
+        <FieldAlerts messages={errors?.description} />
       </Form.Group>
+      <div className="d-flex justify-content-center gap-3 my-4">
+        {/* Cancel button */}
+        <Button
+          className={`${btnStyles.Button} ${btnStyles.Cancel} ${btnStyles.SmallWide} rounded-pill`}
+          onClick={handleCancel}
+        >
+          Cancel
+        </Button>
 
-      {/* Cancel button */}
-      <Button
-        className={`${btnStyles.CallOutArt} ${btnStyles.SmallWide} rounded-pill m-2`}
-        onClick={handleCancel}
-      >
-        Cancel
-      </Button>
-
-      {/* Submit Button */}
-      <Button
-        type="submit"
-        className={`${btnStyles.CallOutTutorial} ${btnStyles.SmallWide}  rounded-pill mx-4 my-5`}
-      >
-        Create
-      </Button>
+        {/* Submit Button */}
+        <Button
+          type="submit"
+          className={`${btnStyles.Button} ${btnStyles.Submit} ${btnStyles.SmallWide}  rounded-pill `}
+        >
+          Create
+        </Button>
+      </div>
     </>
   );
 
   return (
-    <Container className="mt-4">
-      <h2>Upload Artwork</h2>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="text-center">
-          {image ? (
-            <>
-              <figure>
-                <img
-                  src={image}
-                  alt="Preview"
-                  style={{
-                    maxHeight: "300px",
-                    width: "100%",
-                    objectFit: "contain",
-                  }}
-                />
-              </figure>
-              <div>
-                <Form.Label
-                  className="btn btn-primary mt-2"
-                  htmlFor="image-upload"
-                >
-                  Change the Image
-                </Form.Label>
-              </div>
-            </>
-          ) : (
-            <div className="d-flex justify-content-center">
-              <Form.Label htmlFor="image-upload" className="w-100 text-center">
-                Click to upload an image
-              </Form.Label>
-            </div>
-          )}
+    <Form className={styles.ArtWorkForm} onSubmit={handleSubmit}>
+      <section className={uniStyles.RowWrapperBg}>
+        <div className={uniStyles.pageShell}>
+          <Row>
+            <h2>Upload Artwork</h2>
 
-          <Form.Control
-            type="file"
-            id="image-upload"
-            accept="image/*"
-            onChange={handleImageChange}
-            ref={imageInput}
-            className="d-none"
-          />
-          <FieldAlerts messages={errors?.image} />
-        </Form.Group>
+            <Col className="py-2 p-0 p-md-2 d-flex max-width" md={6} lg={7}>
+              <Container
+                className={`${appStyles.Content} ${uniStyles.BorderPeach} ${uniStyles.FlexFill} `}
+              >
+                <Form.Group className="text-center">
+                  {image ? (
+                    <>
+                      <figure>
+                        <img
+                          src={image}
+                          alt="Preview"
+                          style={{
+                            maxHeight: "300px",
+                            width: "100%",
+                            objectFit: "contain",
+                          }}
+                        />
+                      </figure>
+                      <div className="mt-2">
+                        <Form.Label
+                          className={`${btnStyles.Button} ${btnStyles.BtnBasePeach} btn my-auto`}
+                          htmlFor="image-upload"
+                        >
+                          Change the Image
+                        </Form.Label>
+                      </div>
+                    </>
+                  ) : (
+                    <div className="d-flex justify-content-center">
+                      <Form.Label
+                        htmlFor="image-upload"
+                        className={` ${styles.AssestContainer} text-center`}
+                      >
+                        <Asset
+                          src={Upload}
+                          message="Click or tap to upload an image"
+                        />
+                      </Form.Label>
+                    </div>
+                  )}
 
-        {/* Reusable form fields */}
-        {formFields}
-      </Form>
-    </Container>
+                  <Form.Control
+                    type="file"
+                    id="image-upload"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                    ref={imageInput}
+                    className="d-none"
+                  />
+                  <FieldAlerts messages={errors?.image} />
+                </Form.Group>
+              </Container>
+            </Col>
+            {/* Reusable form fields */}
+            <Col sx={2} md={6} lg={5} className="p-0 p-md-2">
+              <Container
+                className={`${appStyles.Content} ${uniStyles.BorderPeach} `}
+              >
+                {formFields}
+              </Container>
+            </Col>
+          </Row>
+        </div>
+      </section>
+    </Form>
   );
 };
 
